54b578d76df71724a858630af0be485a9d5cfb37
luitelshreejal <luitelshreejal@gmail.com>
2021-07-25 13:33:18 -0700

    initial commit
    The formula for Min-max normalization is below:
    
    (value - minimum_value_in_list) / (maximum) - (minimum)

---
+++ k_nearest_neighbor/data_normalization.py
@@ -0,0 +1,12 @@
+release_dates = [1897, 1998, 2000, 1948, 1962, 1950, 1975, 1960, 2017, 1937, 1968, 1996, 1944, 1891, 1995, 1948, 2011, 1965, 1891, 1978]
+
+def min_max_normalize(lst):
+  minimum = min(lst)
+  maximum = max(lst)
+  normalized = []
+
+  for i in range(len(lst)):
+    normalized.append((lst[i] - minimum)/ (maximum - minimum))
+  return normalized
+
+print(min_max_normalize(release_dates))
